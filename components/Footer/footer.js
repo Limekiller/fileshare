4a745a552e999246387d1fb05f751f0497ecef08
Limekiller <me@bryceyoder.com>
2020-10-13 11:03:05 -0400

    Add nav to footer

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import styles from './footer.module.scss'
+import Link from 'next/link'
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <img src="/images/logo.svg" alt="Bryce Yoder"/>
-            <span>Created by Bryce Yoder</span>
+            <div class='flex'>
+                <Link href='/'>
+                    <a><i class="fas fa-home"></i></a>
+                </Link>
+                <Link href='/share'>
+                    <a><i class="fas fa-file-upload"></i></a>
+                </Link>
+                <Link href='/download'>
+                    <a><i class="fas fa-file-download"></i></a>
+                </Link>
+
+            </div>
+            <a href='https://bryceyoder.com'>
+                <img src="/images/logo.svg" alt="Bryce Yoder"/>
+            </a>
         </footer>
     )
 }
