bdb6c34ea25d4b694c216c474fddc2f9251c75d7
= <me@bryceyoder.com>
2020-10-16 22:57:49 -0400

    Add percentage to upload bar

--- components/UploadBox/uploadBox.js
+++ components/UploadBox/uploadBox.js
@@ -8,7 +8,7 @@ export default class UploadBox extends Component {
 
     constructor({props, router}) {
         super(props);
-        this.state = { 
+        this.state = {
             hovered: false,
             files: []
         };
@@ -47,23 +47,25 @@ export default class UploadBox extends Component {
     render() {
         return (
             <>
-                <div id='loadingBar' className={styles.loadingBar}/>
-                <input 
-                    id='fileInput' 
-                    className={ styles.fileInput } 
-                    type="file" 
-                    multiple 
+                <div id='loadingBar' className={styles.loadingBar}>
+                    <h1 id='loadingPercent' className={styles.loadingPercent} />
+                </div>
+                <input
+                    id='fileInput'
+                    className={ styles.fileInput }
+                    type="file"
+                    multiple
                     onChange={e => this.handleFiles()}
                 />
-                <div 
-                    type='file' 
-                    multiple 
+                <div
+                    type='file'
+                    multiple
                     id='uploadBox'
                     className={`
                         flex wrap
-                        ${styles.uploadBox} 
+                        ${styles.uploadBox}
                         ${this.state.hovered ? styles.active : ''}
-                    `} 
+                    `}
                     onDragOver={e => this.handleDragOver(e)}
                     onDragEnter={e => this.handleDragEnter(e)}
                     onDrop={e => this.handleDrop(e)}
@@ -94,7 +96,7 @@ export default class UploadBox extends Component {
         for (let i = 0; i < files.length; i++) {
             filesToMerge.push(files[i]);
         }
-        
+
         this.setState({ files: filesToMerge });
     }
 
@@ -135,6 +137,7 @@ export default class UploadBox extends Component {
 
             socket.on('uploadProgress', percent => {
                 document.querySelector('#loadingBar').style.width = percent + 'vw';
+                document.querySelector('#loadingPercent').innerHTML = percent.toFixed(2) + '%';
             });
         })
     }
